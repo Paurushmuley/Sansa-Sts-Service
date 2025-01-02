e864e5b0ce7f9f7a539f48cca46a19dbcbddbf24
Paurush Muley <paurush.muley@centralogic.net>
2025-01-02 18:25:44 +0530

    update app.js

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ app.get("/", (req, res) => {
     res.send("Hello, World!"); // Respond with a simple message
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
+
 });
